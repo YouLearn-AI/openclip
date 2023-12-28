39e29edd61076ff33a093c94bdae67db3416fa73
Advait Paliwal <advaitspaliwal@gmail.com>
2023-12-27 20:50:18 -0500

    fix errors

--- app.py
+++ app.py
@@ -3,22 +3,22 @@ from openclip import OpenCLIPEmbeddings
 
 def handler(event, context):
 
-    event_payload = event['queryStringParameters']
-    if not ('base64_images' in event_payload or 'texts' in event_payload):
+    if not ('base64_images' in event or 'texts' in event):
         return {
             'headers': {'Content-Type': 'application/json'},
             'statusCode': 400,
             'body': json.dumps({'error': 'No valid data provided in the event'}),
+            'event': event,
         }
     
     clip_embeddings = OpenCLIPEmbeddings()
 
     try:
-        if 'base64_images' in event_payload:
-            base64_images = list(event_payload['base64_images'])
+        if 'base64_images' in event:
+            base64_images = list(event['base64_images'])
             embeddings = clip_embeddings.embed_base64s(base64_images)
-        elif 'texts' in event_payload:
-            texts = list(event_payload['texts'])
+        elif 'texts' in event:
+            texts = list(event['texts'])
             embeddings = clip_embeddings.embed_texts(texts)
 
         return {
@@ -31,4 +31,5 @@ def handler(event, context):
             'headers': {'Content-Type': 'application/json'},
             'statusCode': 500,
             'body': json.dumps({'error': 'Error during embedding: {}'.format(e)}),
+            'event': event,
         }
